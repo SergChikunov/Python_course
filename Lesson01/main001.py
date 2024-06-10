dc094641dc3a757680bcb116687aae8f741c7f6c
Sergey Chikunov <consull@outlook.com>
2024-06-10 20:46:32 +0500

    Python. 1 урок - Знакомство с языком. Условный оператор if->elif->else

---
+++ Lesson01/main001.py
@@ -0,0 +1,10 @@
+username =  input('Введите имя: ')
+if username == 'Маша':
+    print('Ура, это же Маша!')
+elif username == 'Марина':
+    print('Я так ждала Вас, Марина!')
+elif username == 'Ильнар':
+    print('Ильнар - топ)')
+else:
+    print('Привет, ', username)
+    
