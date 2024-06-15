7d0a0bc40ff5b73d295eeac598c944af8e4a5b45
Sergey Chikunov <consull@outlook.com>
2024-06-15 18:43:29 +0500

    Python. Семинар 2. Задача 2, входит ли число в поледовательность Фибоначчи. Добавлены правки и комментарий

--- Seminar02/task02.py
+++ Seminar02/task02.py
@@ -9,22 +9,22 @@ Output: 6
 
 
 a = 5
-fibo_p, fibo_n = 0, 1
-position = 2
+fibo_p, fibo_n = 0, 1  # первые два числа последовательности
+position = 2  # есть первые два числа последовательности, соответсвенно мы на 2 месте последовательности
 while fibo_n < a:
-    fibo_p, fibo_n = fibo_n, fibo_p + fibo_n
+    fibo_p, fibo_n = fibo_n, fibo_p + fibo_n  # определяем последующие члены последовательности
     position += 1
-if fibo_n == a:
+if fibo_n == a:  # определяем что же получилось по итогу решения задачи
     print(position)
 else:
     print(-1)
 
 
 def func(a=5, fibo_p=0, fibo_n=1, position=2):  # рекурсия
-    if fibo_n == a:
+    if fibo_n == a:  # базовый случай
         return position
     elif fibo_n < a:
-        return func(a, fibo_n, fibo_p + fibo_n, position + 1)
+        return func(a, fibo_n, fibo_p + fibo_n, position + 1) # рекурсивный вызов
     else:
         return -1
 
