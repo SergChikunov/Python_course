cfe9311c62024bac365b47b05aa8b5a0e7201263
Sergey Chikunov <consull@outlook.com>
2024-06-22 23:35:51 +0500

    Python. Лекция 3. Рекурсия. Задание 4. Вывод 10 первых членов последовательности Фибоначчи

---
+++ Lesson03/task04.py
@@ -0,0 +1,15 @@
+"""
+Рекурсия. Вывод последовательности Фибоначчи
+"""
+
+
+def fib(n):
+    if n in [1, 2]:  # базовый случай
+        return 1
+    return fib(n - 1) + fib(n - 2)  # рекурсивный вызов
+
+
+list_1 = []
+for i in range(1, 10):
+    list_1.append(fib(i))
+print(list_1)
